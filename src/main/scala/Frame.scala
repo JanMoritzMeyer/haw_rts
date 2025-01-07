aa86ef4946488dea4ae8de4bbd0f7e692750616e
Jan Moritz Meyer <janmoritzmeyer@yahoo.com>
2025-01-07 23:19:25 +0100

    random size

--- src/main/scala/Frame.scala
+++ src/main/scala/Frame.scala
@@ -1,3 +1,5 @@
+import scala.util.Random
+
 case class Frame(stream: String, pcp: Int, minsize: Int, maxsize: Int) {
-  def getSize: Int = (minsize + maxsize) / 2
+  def getSize: Int = Random.between(minsize, maxsize)
 }
